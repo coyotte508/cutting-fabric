0432aad2f0b2f53a14772cd5b3d5ee8b0949ab7d
coyotte508 <coyotte508@gmail.com>
2024-04-06 00:20:25 +0200

    ✨ Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
@@ -39,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +32,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _fabricWidth = 140;
+  final _fabricWidthController = TextEditingController();
+  double _pricePerMeter = 10;
+  final _pricePerMeterController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  var panel = {
+    "width": 0.0,
+    "length": 0.0,
+    "quantity": 1,
+  };
+  final _panelWidthController = TextEditingController();
+  final _panelLengthController = TextEditingController();
+  final _panelQuantityController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fabricWidthController.addListener(() {
+      setState(() {
+        _fabricWidth = int.parse(_fabricWidthController.text);
+      });
     });
+    _pricePerMeterController.addListener(() {
+      setState(() {
+        _pricePerMeter = double.parse(_pricePerMeterController.text);
+      });
+    });
+    _panelWidthController.addListener(() {
+      setState(() {
+        panel["width"] = double.parse(_panelWidthController.text);
+      });
+    });
+    _panelLengthController.addListener(() {
+      setState(() {
+        panel["length"] = double.parse(_panelLengthController.text);
+      });
+    });
+    _panelQuantityController.addListener(() {
+      setState(() {
+        panel["quantity"] = int.parse(_panelQuantityController.text);
+      });
+    });
+    _fabricWidthController.text = '$_fabricWidth';
+    _pricePerMeterController.text = '$_pricePerMeter';
+    _panelWidthController.text = '${panel["width"]}';
+    _panelLengthController.text = '${panel["length"]}';
+    _panelQuantityController.text = '${panel["quantity"]}';
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              keyboardType: const TextInputType.numberWithOptions(),
+              decoration: const InputDecoration(
+                label: Text('Largeur de tissu en cm'),
+              ),
+              onChanged: (value) => setState(() {
+                _fabricWidth = int.parse(value);
+              }),
+              controller: _fabricWidthController,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              keyboardType: const TextInputType.numberWithOptions(),
+              decoration: const InputDecoration(
+                label: Text('Prix au mètre en €'),
+              ),
+              onChanged: (value) => setState(() {
+                _pricePerMeter = double.parse(value);
+              }),
+              controller: _pricePerMeterController,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                    child: TextField(
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  decoration: const InputDecoration(
+                    label: Text('Longueur en cm'),
+                  ),
+                  onChanged: (value) => setState(() {}),
+                  controller: _panelLengthController,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                )),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: TextField(
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  decoration: const InputDecoration(
+                    label: Text('Largeur en cm'),
+                  ),
+                  onChanged: (value) => setState(() {}),
+                  controller: _panelWidthController,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                )),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: TextField(
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  decoration: const InputDecoration(
+                    label: Text('Quantité'),
+                  ),
+                  onChanged: (value) => setState(() {}),
+                  controller: _panelQuantityController,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                )),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
